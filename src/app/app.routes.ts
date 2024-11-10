113c0c8f2c2bf181cabb96f11c86bd129a93cbf1
joaquinSolano <Jsolanoupc04@gmail.com>
2024-11-10 00:20:40 -0500

    v0.15.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioRegistrarComponent } from './components/usuario/usuario-registrar/usuario-registrar.component';
 import { RolComponent } from './components/rol/rol.component';
 import { RolRegistrarComponent } from './components/rol/rol-registrar/rol-registrar.component';
 
@@ -8,8 +7,7 @@ export const routes: Routes = [
     {
         path: 'usuarios', component:UsuarioComponent,
         children:[
-            {path: 'nuevo',component:UsuarioRegistrarComponent},
-            {path:'ediciones/:id',component:UsuarioRegistrarComponent}
+
         ]
     },
     {
